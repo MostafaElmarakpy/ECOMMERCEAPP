5cbcb9ceb048650ca59e62e889527a414c9107ce
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-08-25 08:53:46 +0300

    Refactor app component structure; remove placeholder content and update imports for header, footer, and products components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from './components/footer/footer.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule,
+    // RouterOutlet,
+    HeaderComponent,
+    FooterComponent,
+    ProductsComponent
+
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'ECOMMERCEAPP';
