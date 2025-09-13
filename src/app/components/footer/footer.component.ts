4e5f83ed3106a7b32bf3bdcf10c68ab28203ac00
Mostafa Elmarakpy <mostafaelmarakpy1@gmail.com>
2025-09-13 19:49:36 +0300

    Refactor footer component: update HTML structure, add responsive design, and implement data emission to parent component

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { ɵEmptyOutletComponent } from "../../../../node_modules/@angular/router/router_module.d-Bx9ArA6K";
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [ɵEmptyOutletComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  // create input property to receive data from parent
+  @Input() recevedDataFromApp:string="";
 
+  // create event emitter to send data from child to parent
+  @Output() sendDataToParent = new EventEmitter<string>();
+  sendData() {
+    const data = "Data from Footer Component";
+    this.sendDataToParent.emit(data);
+  }
 }
